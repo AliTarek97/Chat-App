460b6864ff6bf53f5ee4557524559d46f2369c32
Ali <aly_helmy2015@hotmail.com>
2020-07-14 22:25:43 +0200

    Timestamps for Location Messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,10 +13,11 @@ const locationMessageTemplate = document.querySelector(
   "#location-message-template"
 ).innerHTML;
 
-socket.on("locationMessage", (url) => {
-  console.log(url);
+socket.on("locationMessage", (message) => {
+  console.log(message);
   const html = Mustache.render(locationMessageTemplate, {
-    url,
+    url: message.url,
+    createdAt: moment(message.createdAt).format("h:mm a"),
   });
   $messages.insertAdjacentHTML("beforeend", html);
 });
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const Filter = require("bad-words");
-const { generateMessage } = require("./utils/messages");
+const {
+  generateMessage,
+  generateLocationMessage,
+} = require("./utils/messages");
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -38,7 +41,9 @@ io.on("connection", (socket) => {
   socket.on("sendLocation", (coords, callback) => {
     io.emit(
       "locationMessage",
-      `https://google.com/maps/?q=${coords.latitude},${coords.longitude}`
+      generateLocationMessage(
+        `https://google.com/maps/?q=${coords.latitude},${coords.longitude}`
+      )
     );
     callback();
   });
