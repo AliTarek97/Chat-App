9682b1ba32dc406e49c74159b92b9fcbc8d141a0
Ali <aly_helmy2015@hotmail.com>
2020-07-14 21:55:14 +0200

    location message template

--- src/index.js
+++ src/index.js
@@ -1,51 +1,53 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const Filter = require('bad-words');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
 
 const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname , '../public');
+const publicDirectoryPath = path.join(__dirname, "../public");
 
 const app = express();
 // if we don't do this the express library does this behind the scene anyway
-// we do this to pass server as param to socketio 
+// we do this to pass server as param to socketio
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(express.static(publicDirectoryPath));
 
-
 // socket has information about any new connection
 // connection and disconnect are built in connections
-io.on('connection' , (socket) => {
-    console.log('New webSocket connection');
+io.on("connection", (socket) => {
+  console.log("New webSocket connection");
 
-    socket.emit('message' , 'Welcome!');
-    // it will emit to everbody except that particular connection
-    socket.broadcast.emit('message' , 'A new user has joined!');
+  socket.emit("message", "Welcome!");
+  // it will emit to everbody except that particular connection
+  socket.broadcast.emit("message", "A new user has joined!");
 
-    socket.on('sendMessage' , (message , callback) => {
-        const filter = new Filter();
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter();
 
-        if(filter.isProfane(message)){
-            return callback('Profanity is not allowed')
-        }
-        io.emit('message' , message);
-        callback();
-    });
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed");
+    }
+    io.emit("message", message);
+    callback();
+  });
 
-    socket.on('sendLocation' , (position , callback) => {
-        io.emit('message' , `https://google.com/maps/?q=${position.latitude},${position.longitude}`);
-        callback();
-    })
+  socket.on("sendLocation", (coords, callback) => {
+    io.emit(
+      "locationMessage",
+      `https://google.com/maps/?q=${coords.latitude},${coords.longitude}`
+    );
+    callback();
+  });
 
-    socket.on('disconnect' , () => {
-        //we user io.emit because I have been already disconnected
-        io.emit('message' , 'A user has left')
-    });
-})
+  socket.on("disconnect", () => {
+    //we user io.emit because I have been already disconnected
+    io.emit("message", "A user has left");
+  });
+});
 
-server.listen(port , () => {
-    console.log(`Server is up on port ${port}`);
-})
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
